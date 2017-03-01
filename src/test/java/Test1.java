96a3270c048bcac17ceaa97a0e93253c31cf050d
GREAUX Thomas <thomas.greaux@etu.unice.fr>
2017-03-01 11:13:17 +0100

    Initial code

---
+++ src/test/java/Test1.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+/**
+ * Created by Thoma on 3/1/2017.
+ */
+public class Test1 {
+
+    @Test
+    public void test(){
+        assertTrue(true);
+    }
+}
