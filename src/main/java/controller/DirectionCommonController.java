e2335668b8434796c730f12b454075e04ee88d4f
Jeremy Lara <jeremy.lara@etu.unice.fr>
2017-03-03 15:33:38 +0100

    Correction problème d'id de controller

---
+++ src/main/java/controller/DirectionCommonController.java
@@ -0,0 +1,84 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Separator;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class DirectionCommonController {
+
+    @FXML
+    private HBox directionCommon;
+
+    @FXML
+    private VBox coordContainer;
+
+    @FXML
+    private Label nosCoordonnees;
+
+    @FXML
+    private VBox coordExpliciteContainer;
+
+    @FXML
+    private HBox numTelContainer;
+
+    @FXML
+    private Label numTel;
+
+    @FXML
+    private Label numTelExplicite;
+
+    @FXML
+    private HBox adresseContainer;
+
+    @FXML
+    private Label adresse;
+
+    @FXML
+    private Label adresseExplicite;
+
+    @FXML
+    private HBox webSiteContainer;
+
+    @FXML
+    private Label webSite;
+
+    @FXML
+    private Label webSiteExplicit;
+
+    @FXML
+    private VBox lineContainer;
+
+    @FXML
+    private Separator separator;
+
+    @FXML
+    private VBox infoDirectionContainer;
+
+    @FXML
+    private Label ouNousTrouver;
+
+    @FXML
+    private HBox directionLabelContainer;
+
+    @FXML
+    private Label cliquezBouton;
+
+    @FXML
+    private Label nomBouton;
+
+    @FXML
+    private HBox seDirigerContainer;
+
+    @FXML
+    private Button seDirigerBouton;
+
+    @FXML
+    void directionsPage(ActionEvent event) {
+
+    }
+
+}
